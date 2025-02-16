0037fb33cfe49ffa13b6404e1cb25cbb1302dcd0
Pranav-Pote-42 <221142@iiitt.ac.in>
2025-02-16 13:14:55 +0530

    commit_16_02_pote

--- backend/src/middleware/auth.middleware.js
+++ backend/src/middleware/auth.middleware.js
@@ -1,26 +1,26 @@
 import jwt from "jsonwebtoken"
 import User from "../models/user.model.js"
 
-export const protectRoute= async (req,res)=>{
+export const protectRoute= async (req,res,next)=>{
     const token=req.cookies.jwt;
 
     try {
 
         if(!token){
-        res.status(401).json({message:"Unauthorized - No token Provided"})
+        return res.status(401).json({message:"Unauthorized - No token Provided"})
     }
 
     const decoded=jwt.verify(token,process.env.JWT_SECRET)
-    console.log(decoded)
+
 
     if(!decoded){
-        res.status(401).json({message:"unauthorized Invalid token"})
+        return res.status(401).json({message:"unauthorized Invalid token"})
     }
 
     const user=await User.findById(decoded.userId).select("-password");
 
     if(!user){
-        res.status(404).json({message:"user not found"})
+        return res.status(404).json({message:"user not found"})
     }
 
     req.user=user
@@ -29,7 +29,7 @@ export const protectRoute= async (req,res)=>{
         
     } catch (error) {
         console.log("error in protect route");
-        res.status(500).json({message:"internal srver error"});
+        res.status(500).json({message:"internal srver error from protect Route"});
     }
 
 
--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,5 +1,5 @@
 import express from "express"
-import { login, logout, signup } from "../controllers/auth.controllers.js";
+import { checkAuth, login, logout, signup } from "../controllers/auth.controllers.js";
 import { protectRoute } from "../middleware/auth.middleware.js";
 import { updateProfile } from "../controllers/auth.controllers.js";
 
@@ -13,6 +13,8 @@ router.post("/login",login);
 router.post("/logout",logout);
 
 router.put("/update-profile",protectRoute,updateProfile)
+
+router.get("/check",protectRoute,checkAuth)
 
 
 
