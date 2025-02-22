37543aee4220488324b4e4c841df08b8c82da93c
Pranav-Pote-42 <221142@iiitt.ac.in>
2025-02-22 12:56:35 +0530

    commit_22_02_pote

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,13 +4,16 @@ import cookieParser from "cookie-parser"
 
 import {connectDB} from "./lib/db.js"
 import authRoutes from "./routes/auth.route.js"
+import messageRoutes from "./routes/message.route.js"
 
 dotenv.config()
 const app=express();
 
 app.use(express.json())
 app.use(cookieParser())
+
 app.use("/api/auth",authRoutes)
+app.use("/api/message",messageRoutes)
 
 const PORT=process.env.PORT;
     
